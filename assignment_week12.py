e91b8e9ae25e6bde249daf9b744470ac512523d4
Saatwik Semwal <saatwik.semwal@intimetec.com>
2026-02-24 16:55:40 +0530

    Assignment added

---
+++ assignment_week12.py
@@ -0,0 +1,18 @@
+#Write a program to find common values from two list
+
+
+#Write a program to remove duplicate elements from the given list with the original order.
+
+#Write a program to sort the list, dictionary. 
+
+#Write a program to convert decimal number into octal and vice versa.
+
+#Write a program to print all prime numbers between 0, 10 using list comprehension.
+
+#Write a method to take a list as an input and gives the output as a list of tuples. where each tuple will contain (index, value).
+#example: input = [2,3,4,5,6]
+#output = [(0,2), (1,3), (2,4), (3, 5), (4, 6)]
+
+#Program to accept a matrix of order MxM and interchange the diagonals.
+
+#Program to accept a text file as input and print the text in a new file with the first alphabet of each word capitalized. Also, print the number of white spaces in the new file. 
